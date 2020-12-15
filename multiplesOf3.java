79e436e79e282d827dd2264e3bb45012da1977a5
Owen1k2 <owenhblock@gmail.com>
2020-12-15 16:34:03 -0500

    Multiples of 3 Day 1

---
+++ multiplesOf3.java
@@ -0,0 +1,13 @@
+public class multiplesOf3 {
+    public static void main(String[] args) {
+        
+        for(int x=0; x<100; x+=3) {
+            System.out.println(x);
+        }
+
+    }
+}
+
+
+// We need numbers between 0 and 100 that are multiples of 3
+// Numbers must be x>0 x<100
