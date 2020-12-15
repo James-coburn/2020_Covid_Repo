c829f2cec8998987657582cc53c14e7a10f7e4ed
Camden Bartlo <camdenbartlo@Camdens-MacBook-Air.local>
2020-12-15 16:23:50 -0500

    Created the program.

---
+++ Threes.java
@@ -0,0 +1,11 @@
+public class Threes {
+    public static void main(String[] args) {
+        for (int i = 0; i < 900; i++) {
+            if (i % 3 == 0) {
+                System.out.print(i + ", ");
+            } else {
+                System.out.print("");
+            }
+        }
+    }
+}
